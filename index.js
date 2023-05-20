bade5f9a3678148c741ef901a29d16a3bc19b767
abhijit_unified <abhijit.shaw@unifiedinfotech.net>
2023-05-21 03:13:32 +0530

    projrect setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+
+
+
+app.listen(port,function(error){
+   if(error){
+    console.log(`Error occured at ${error}`);
+   }
+   console.log(`Server is up and running at port : ${port}`);
+})
